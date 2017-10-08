b3148a7a78969247947cd90e43d409f9f18f08e2
Akhil Soman <akhil.soman@ge.com>
2017-10-08 16:24:56 +0530

    Adding gallery access

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ImagePicker } from '@ionic-native/image-picker';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ImagePicker]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  private imageSrc: string;
+  constructor(
+    public navCtrl: NavController,
+    public imagePicker: ImagePicker
+  ) {}
+  
+  private openGallery () : void {
+    let options = {
+       maximumImagesCount: 1,
+       width: 800,
+       height: 800,
+       quality: 100,
+       correctOrientation: true
+      };
+    this.imagePicker.getPictures(options).then((file) => {
+        this.imageSrc = file;
+      }, (err) => { });
   }
 
 }
