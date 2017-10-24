890811ab1377199e2a26de8d5c728ac27e366574
Akhil Soman <akhil.soman@ge.com>
2017-10-24 16:57:11 +0530

    Adding camera plugin and gallery access

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,31 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ImagePicker } from '@ionic-native/image-picker';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [ImagePicker]
+  providers: [Camera]
 })
 export class HomePage {
 
   private imageSrc: string;
+
   constructor(
     public navCtrl: NavController,
-    public imagePicker: ImagePicker
+    public camera: Camera,
+    public zone : NgZone
   ) {}
-  
-  private openGallery () : void {
-    let options = {
-       maximumImagesCount: 1,
-       width: 800,
-       height: 800,
-       quality: 100,
-       correctOrientation: true
-      };
-    this.imagePicker.getPictures(options).then((file) => {
-        this.imageSrc = file;
-      }, (err) => { });
-  }
 
+  private openGallery () : void {
+    // let options = {
+    //    maximumImagesCount: 1,
+    //    width: 800,
+    //    height: 800,
+    //    quality: 100,
+    //    correctOrientation: true
+    //   };
+
+  this.camera.getPicture({
+     sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+     destinationType: this.camera.DestinationType.DATA_URL,
+     encodingType: this.camera.EncodingType.JPEG
+    }).then((imageData) => {
+      this.zone.run(() => {
+        this.imageSrc = 'data:image/jpeg;base64,'+imageData;
+        console.log(this.imageSrc)
+      })
+     }, (err) => {
+      console.log(err);
+    });
+  }
 }
